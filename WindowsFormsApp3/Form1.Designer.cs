1afbeee4e37ebc5e0121f654bf42df9ea6cc6f4d
fukur <fukur@LAPTOP-7EQMGM6H>
2020-05-07 20:44:51 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.A = new System.Windows.Forms.TextBox();
+            this.Rez = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.C = new System.Windows.Forms.TextBox();
+            this.B = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Сторона треугольника а:";
+            // 
+            // A
+            // 
+            this.A.Location = new System.Drawing.Point(265, 42);
+            this.A.Name = "A";
+            this.A.Size = new System.Drawing.Size(238, 22);
+            this.A.TabIndex = 1;
+            // 
+            // Rez
+            // 
+            this.Rez.Location = new System.Drawing.Point(52, 147);
+            this.Rez.Name = "Rez";
+            this.Rez.Size = new System.Drawing.Size(451, 22);
+            this.Rez.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(451, 49);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(172, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Сторона треугольника в:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(49, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(172, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Сторона треугольника с:";
+            // 
+            // C
+            // 
+            this.C.Location = new System.Drawing.Point(265, 108);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(238, 22);
+            this.C.TabIndex = 7;
+            // 
+            // B
+            // 
+            this.B.Location = new System.Drawing.Point(265, 74);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(238, 22);
+            this.B.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 285);
+            this.Controls.Add(this.B);
+            this.Controls.Add(this.C);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Rez);
+            this.Controls.Add(this.A);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox A;
+        private System.Windows.Forms.TextBox Rez;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox C;
+        private System.Windows.Forms.TextBox B;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(A.Text);
+            double b = Convert.ToDouble(B.Text);
+            double c = Convert.ToDouble(C.Text);
+            if((a + b > c) && (b + c > a) && (c + a > b))
+            Rez.Text = Convert.ToString("Такой треугольник существует");
+            else Rez.Text = Convert.ToString("Такой треугольник не существует");
+
+        }
+    }
+}
